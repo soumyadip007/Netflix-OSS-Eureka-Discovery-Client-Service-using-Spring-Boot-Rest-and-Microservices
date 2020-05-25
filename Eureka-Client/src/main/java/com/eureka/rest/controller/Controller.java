6a36f3804697f0a59c029211ac26092374be3d1b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 13:34:48 +0530

    Controller em

--- Eureka-Client/src/main/java/com/eureka/rest/controller/Controller.java
+++ Eureka-Client/src/main/java/com/eureka/rest/controller/Controller.java
@@ -1,14 +1,11 @@
 package com.eureka.rest.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.eureka.enitity.CatalogItem;
 import com.eureka.enitity.UserRating;
 
 @RestController
