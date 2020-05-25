b35d0a371f173ab1508df4c857cea0bfa7121862
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 14:20:48 +0530

    WebBuilder Added

--- Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
+++ Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,5 +20,10 @@ public class EurekaClientApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
 	}
 }
