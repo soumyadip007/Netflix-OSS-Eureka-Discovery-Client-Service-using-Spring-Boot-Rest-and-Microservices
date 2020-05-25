c2b952236c6118c030830c484e36b3e5b0e80a65
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 14:43:22 +0530

    @EnableAync

--- Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
+++ Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
@@ -2,13 +2,19 @@ package com.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAsync
+@ComponentScan({"com.eureka.*"})
+@EntityScan("com.eureka.*")
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
