591858e892421666ee9378d0f70c9ace93db63d9
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 14:35:27 +0530

    webflux -rm

--- Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
+++ Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -20,10 +19,5 @@ public class EurekaClientApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
-	}
-	
-	@Bean
-	public WebClient.Builder getWebClientBuilder(){
-		return WebClient.builder();
 	}
 }
