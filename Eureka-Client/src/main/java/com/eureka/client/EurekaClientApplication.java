800259831be77af8d75f45106e999e322a8707c7
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 13:30:26 +0530

    Client application

--- Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
+++ Eureka-Client/src/main/java/com/eureka/client/EurekaClientApplication.java
@@ -2,8 +2,10 @@ package com.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
