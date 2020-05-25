6991999db5746466713412f57a61be1be641f862
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 13:33:01 +0530

    Controller

--- Eureka-Client/src/main/java/com/eureka/rest/controller/Controller.java
+++ Eureka-Client/src/main/java/com/eureka/rest/controller/Controller.java
@@ -1,10 +1,31 @@
 package com.eureka.rest.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.eureka.enitity.CatalogItem;
+import com.eureka.enitity.UserRating;
 
 @RestController
 @RequestMapping("eureka")
 public class Controller {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+
+    @RequestMapping("/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+
+        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+
+        return userRating;
+
+    }
+	
 }
